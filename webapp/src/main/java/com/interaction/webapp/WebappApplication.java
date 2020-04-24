3e6c19c67db61ef603a1850760df214aa0634c06
faiana <biafriedrich06@gmail.com>
2020-04-24 13:03:17 +0100

    heroku test

--- webapp/src/main/java/com/interaction/webapp/WebappApplication.java
+++ webapp/src/main/java/com/interaction/webapp/WebappApplication.java
@@ -11,8 +11,8 @@ public class WebappApplication {
 
     public static void main(String[] args) {
         /*Getting MongoDB URL via environment variable*/
-        Map<String, String> env = System.getenv();
-        String mongoUrl= env.get("MONGODB_URL");
+//        Map<String, String> env = System.getenv();
+//        String mongoUrl= env.get("MONGODB_URL");
 //        System.out.println(mongoUrl);
         SpringApplication.run(WebappApplication.class, args);
     }
