7e44b6546e88fdd900ee53ce884c6b668a0a1003
faiana <biafriedrich06@gmail.com>
2020-04-24 23:33:03 +0100

    Fixing the path on server.js to access webapp-ui folder

--- webapp-ui/server.js
+++ webapp-ui/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/webapp-ui'));
 app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html')
+  res.sendFile(__dirname + '/dist/webapp-ui/index.html')
 })
 
 app.listen(process.env.PORT || 4200);
