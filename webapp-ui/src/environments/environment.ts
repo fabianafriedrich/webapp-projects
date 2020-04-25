ffef5fceb9338d8d97f81ad3841fbc48dd0cf121
faiana <biafriedrich06@gmail.com>
2020-04-25 10:54:19 +0100

    Dev Enironment config rollback

--- webapp-ui/src/environments/environment.ts
+++ webapp-ui/src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:8080'
-  apiUrl: 'https://cct-iwa-webapp-api.herokuapp.com'
+  apiUrl: 'http://localhost:8080'
 };
 
 /*
