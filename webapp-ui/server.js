d40d77e2016880339388db35f910f7a651e7d83d
faiana <biafriedrich06@gmail.com>
2020-04-25 09:07:26 +0100

    setting headers

--- webapp-ui/server.js
+++ webapp-ui/server.js
@@ -3,8 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/webapp-ui'));
 app.get('/*', function (req, res) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.sendFile(__dirname + '/dist/webapp-ui/index.html')
