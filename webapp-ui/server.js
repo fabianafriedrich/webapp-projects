1802bf2cb9cd44d6b01524a9013f9c0d47874329
faiana <biafriedrich06@gmail.com>
2020-04-25 09:00:27 +0100

    fixing origin issue adding headers

--- webapp-ui/server.js
+++ webapp-ui/server.js
@@ -5,6 +5,8 @@ app.use(express.static(__dirname + '/dist/webapp-ui'));
 app.get('/*', function (req, res) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.sendFile(__dirname + '/dist/webapp-ui/index.html')
 })
 
