5678f3cb70e4e80ff8a7d37a9e39688b93309c21
faiana <biafriedrich06@gmail.com>
2020-04-25 09:27:44 +0100

    config cors

--- webapp-ui/server.js
+++ webapp-ui/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+
+// use it before all route definitions
+app.use(cors({origin: '*'}));
 app.use(express.static(__dirname + '/dist/webapp-ui'));
 app.get('/*', function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.sendFile(__dirname + '/dist/webapp-ui/index.html')
 })
 
