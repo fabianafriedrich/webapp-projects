ff4d9ae0894e4cfcac17994387f6a5c65db6a4c8
faiana <biafriedrich06@gmail.com>
2020-04-25 08:44:04 +0100

    fixing origin issue

--- webapp-ui/server.js
+++ webapp-ui/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/webapp-ui'));
 app.get('/*', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.sendFile(__dirname + '/dist/webapp-ui/index.html')
 })
 
